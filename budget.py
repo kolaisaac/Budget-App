ab4a5e0a754b5baca0062f173e39d847c00ef16c
kolaisaac <blestisaac10@gmail.com>
2021-04-17 13:33:25 +0100

    Function Decleration

---
+++ budget.py
@@ -0,0 +1,14 @@
+class Category:
+    def __init__():
+        
+    def depost_fund():
+
+    def withdraw_fund():
+
+    def compute_balance():
+
+    def transfering_balance():
+
+
+def display_spent_chart(categories):
+    return "**** real ****"
